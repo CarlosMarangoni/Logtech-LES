26469364d3048c4f3714ed2e94e432e56ad96b4d
Carlos <carlos.marangoni1@gmail.com>
2021-10-04 11:06:00 -0300

    First commit

---
+++ backend/src/main/java/br/com/logtech/logtech/LogtechApplication.java
@@ -0,0 +1,13 @@
+package br.com.logtech.logtech;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LogtechApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LogtechApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/br/com/logtech/logtech/LogtechApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.logtech.logtech;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LogtechApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
